072692c74411349a35f4196d7ad65197abb12563
Marco Jochum <marco.jochum@tum.de>
2024-07-11 10:50:03 +0200

    Fix: Adpated make_sequence function to work for LSTM

--- project/lib/data.py
+++ project/lib/data.py
@@ -1,9 +1,9 @@
 import torch
 from tqdm import tqdm   
+import numpy as np
 
 
-
-def make_Sequence(data, seq_len, next_step):
+def make_sequence(data, seq_len, next_step):
     """
     Generate time-delay sequence data 
     Generates X and Y such that they do not overlap
@@ -20,21 +20,23 @@ def make_Sequence(data, seq_len, next_step):
     
 
     if len(data.shape) <=2:
-        data    = data.unsqueeze(0)
-    #seqLen      = cfg.in_dim
+        data    = np.expand_dims(data, axis=0)
+    nConfigs    = data.shape[0]
     nSamples    = (data.shape[1]-seq_len)
-    X           = torch.empty([nSamples, seq_len, data.shape[-1]])
+    X           = np.empty([nConfigs,nSamples, seq_len, data.shape[-1]])
     #TODO: Adapth the output shape depending on wheter error should be computed
     # at every itermediate step or only for the prediction steps
-    Y           = torch.empty([nSamples, next_step,data.shape[-1]])
+    Y           = np.empty([nConfigs, nSamples, seq_len,data.shape[-1]])
     # Fill the input and output arrays with data
-    k = 0
-    for i in tqdm(torch.arange(data.shape[0])):
-        for j in torch.arange(data.shape[1]-seq_len- next_step):
-            X[k] = data[i, j        :j+seq_len]
-            #TODO: Similarly to above this would also need to be adapted
-            Y[k] = data[i, j+seq_len :j+seq_len+next_step]
-            k    = k + 1
+    
+    for i in tqdm(np.arange(data.shape[0])):
+        k = 0
+        for j in np.arange(data.shape[1]-seq_len- next_step):
+                X[i,k] = data[0,j        :j+seq_len]
+                #TODO: Similarly to above this would also need to be adapted
+                Y[i,k] = data[0, j+next_step :j+seq_len+next_step]
+                k    = k + 1
+    
     print(f"The training data has been generated, has shape of {X.shape, Y.shape}")
 
     return X, Y
