6d2baca5142c7fb3c690a593dcd22489511f3330
Marco Jochum <marco.jochum@tum.de>
2024-07-31 13:43:14 +0200

    added parameters encoded in latent space

--- project/train_lstm.py
+++ project/train_lstm.py
@@ -2,11 +2,21 @@ from lib.train import *
 from lib.data_loading import *
 from NNs.RNN import *
 from lib.data import *
+from NNs.autoencoder import *
 import torch.utils.data as data
+import random
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+set_seed(42)
+
 ##Call ae model to encode all the time series for the different configs
-torch.device("cuda" if torch.cuda.is_available() else "cpu")
-data_folder = '../../data_kmc/'
-suffix = "local_density_li+.txt"
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 n_prod = 50
 n_x = 50
@@ -14,46 +24,101 @@ n_y = 1
 n_z= 100
 n_time = 1000
 ###
-n_epochs = 50
-batch_size = 200
-latent_dim = 40
-# train_data= DataLoading(data_folder, n_prod, n_time, n_x, n_y, n_z,suffix)
-# data = train_data.make_data_set()
-# folder_list = train_data.make_folder_list()
+n_epochs = 400
+batch_size = 4000
+latent_dim = 50
+base = 8
+part_class = 1
 
-lstm = LSTMs(latent_dim, d_model=latent_dim, hidden_size=80, num_layers=2, num_classes=1, embed=None)
-folder_name ="../../data_kmc/2d_encoded/"
+##Initialize VAE for encodings
+encoder = pro_encoder2d(part_class,base, latent_dim)
+decoder = pro_decoder2d(part_class,base, latent_dim)
+##Switch to enforcing params in the vae
+vae = VAE(encoder, decoder, latent_dim=latent_dim, mode='eval')
+vae.load_state_dict(torch.load('model_vae_p.pth', map_location=torch.device(device)))
+vae.eval()
+
+## LOad trainig data
+x_train = torch.tensor(np.load('../../data_kmc/2d_sets/train_set_80_20.npy'), dtype=torch.float32)
+x_test = torch.tensor(np.load('../../data_kmc/2d_sets/test_set_80_20.npy'), dtype=torch.float32)
+
+test_params = get_config("../../data_kmc/2d_sets/test_set_80_20_list.txt")
+train_params = get_config("../../data_kmc/2d_sets/train_set_80_20_list.txt")##epsr, cbulk, v_bias
+
+x_train = x_train[:,::10]/torch.mean(x_train)   
+x_test = x_test/torch.mean(x_train)
+##Rescale parameters with the mean
+params_scale = torch.mean(train_params, dim=0)
+train_params = train_params/params_scale
+test_params = test_params/params_scale
+
+test_params = torch.repeat_interleave(test_params, 1000, dim=0)
+train_params = torch.repeat_interleave(train_params, 100, dim=0)
+
+train_params = torch.reshape(train_params, (x_train.shape[0],x_train.shape[1], train_params.shape[-1] ) )
+test_params = torch.reshape(test_params, (x_test.shape[0],x_test.shape[1], test_params.shape[-1] ) )
+
+
+
+list_x_train = []
+list_x_test = []  
+
+## add params to encoding
+with torch.no_grad():
+    for i in range(len(x_train)):
+        x = x_train[i]  # Add batch dimension
+        params = train_params[i]  # Add batch dimension
+        _, x_lat, _, _ = vae(x, params)
+        list_x_train.append(x_lat)
+    for i in range(len(x_test)): 
+        y = x_test[i]  # Add batch dimension
+        params = test_params[i]  # Add batch dimension
+        _, y_lat, _, _ = vae(y, params)
+        list_x_test.append(y_lat)
+
+x_train_lat =torch.stack(list_x_train)
+x_test_lat = torch.stack(list_x_test)
+
+
+
+
+lstm = LSTMs(latent_dim, d_model=latent_dim, n_mode=1, hidden_size=50, num_layers=1, num_classes=1, embed=None)
 lstm.to(device)
-encodings = loading_encodings(folder_name)
-print(encodings[1].shape)
-holdout = encodings[-5]
-encodings = encodings[:-5]
+print("LSTM parameter count:", sum(p.numel() for p in lstm.parameters() if p.requires_grad))
+x_train_seq, y_train_seq = make_sequence(x_train_lat, 10, 1)
+x_test_seq, y_test_seq = make_sequence(x_test_lat, 10, 1)
 
-train_size = int(encodings.shape[1] * 0.67)    
-test_size = encodings.shape[1] - train_size
-train, test = encodings[:,0:train_size], encodings[:,train_size:]
-print(train.shape)
-print(test.shape)
-x_train, y_train = make_sequence(train, 64, 1)
-x_test, y_test = make_sequence(test, 64, 1)
+# time = torch.tensor(torch.logspace(-8, -4, 1000))
+# time = time[:500]
+# time = time.reshape(time.shape[0], 1)
+# time ,_ = make_sequence(time, 64, 1)
+# time_test = torch.stack([time for i in range( x_test_seq.shape[0])]).to(device) 
+# time = torch.stack([time for i in range( x_train_seq.shape[0])]).to(device)
 
-x_test = torch.tensor(x_test, dtype=torch.float32, device=device)
-y_test = torch.tensor(y_test, dtype=torch.float32, device=device)
-x_train = torch.tensor(x_train, dtype=torch.float32, device=device)
-y_train = torch.tensor(y_train, dtype=torch.float32, device=device)
-x_train = torch.reshape(x_train, (x_train.shape[0]*x_train.shape[1], x_train.shape[2], x_train.shape[3]))
-y_train = torch.reshape(y_train, (y_train.shape[0]*y_train.shape[1], y_train.shape[2], y_train.shape[3]))
-x_test = torch.reshape(x_test, (x_test.shape[0]*x_test.shape[1], x_test.shape[2], x_test.shape[3]))
-y_test = torch.reshape(y_test, (y_test.shape[0]*y_test.shape[1], y_test.shape[2], y_test.shape[3]))
-print("x_train shape:", x_train.shape)
-print("y_train shape:",y_train.shape)
-train_loader = torch.utils.data.DataLoader(data.TensorDataset(x_train, y_train), batch_size=batch_size, 
+# time = time.reshape(time.shape[0]*time.shape[2], 64,1)
+# time_test = time_test.reshape(time_test.shape[0]*time_test.shape[2], 64,1)
+
+x_train_seq = torch.reshape(x_train_seq, (x_train_seq.shape[0]*x_train_seq.shape[1], x_train_seq.shape[2], x_train_seq.shape[3])).to(device)
+y_train_seq = torch.reshape(y_train_seq, (y_train_seq.shape[0]*y_train_seq.shape[1], y_train_seq.shape[2], y_train_seq.shape[3])).to(device)
+x_test_seq = torch.reshape(x_test_seq, (x_test_seq.shape[0]*x_test_seq.shape[1], x_test_seq.shape[2], x_test_seq.shape[3])).to(device)
+y_test_seq = torch.reshape(y_test_seq, (y_test_seq.shape[0]*y_test_seq.shape[1], y_test_seq.shape[2], y_test_seq.shape[3])).to(device)
+
+
+train_loader = torch.utils.data.DataLoader(data.TensorDataset(x_train_seq[:-10], y_train_seq[:-10]), batch_size=batch_size, 
                                            shuffle=True)
+val_loader = torch.utils.data.DataLoader(data.TensorDataset(x_train_seq[-10:], y_train_seq[-10:]), batch_size=10000, 
+                                         shuffle=True)
 
+#time_val = time[-10000:]
 criterion = nn.MSELoss(reduction='mean')
 optimizer = torch.optim.Adam(lstm.parameters(), lr=0.001)
-train_lstm(lstm, criterion, optimizer, train_loader, n_epochs, x_test, y_test)
-lstm.cpu() # move the model to the cpu
-torch.save(lstm.state_dict(), 'model_lstm.pth')
-lstm.eval()
-lstm.load_state_dict(torch.load('model_lstm.pth'))
+
+best_model=train_lstm(lstm, criterion, optimizer, train_loader, n_epochs, val_loader)
+#lstm.cpu() # move the model to the cpu
+torch.save(lstm.state_dict(), 'model_lstm_p24.pth')
+torch.save(best_model, 'model_lstm_val_p24.pth')
+
+with torch.no_grad():
+                y_pred = lstm(x_test_seq)[:,-1,:]
+                loss_test = criterion(y_pred, y_test_seq[:,-1,:])
+                print('Test loss:', loss_test.item()/len(y_test_seq))
--- project/train_vae.py
+++ project/train_vae.py
@@ -1,58 +1,113 @@
 import torch.nn as nn
 import numpy as np
 import torch
+import torch.utils
 from NNs.autoencoder import *
 from lib.train import *
-import os
+from lib.helper import *
+import random
+from itertools import product
+from lib.data_loading import *
+from torch.utils.tensorboard import SummaryWriter
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+set_seed(42)
 
-from torch.utils.data import TensorDataset, random_split
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-torch.manual_seed(42) # Setting the seed
 
+##Hyperparameters:
 part_class = 1
-batch_size=2000
-latent_dim = 40
-base = 32
-data = torch.tensor(np.load('../../data_kmc/2d_sets/2d_red_5.npy'), dtype=torch.float32)
+batch_size=4000
+latent_dim = 10
+base = 8
+num_epochs = 1000
+
+parameters = dict(  
+    latent_dim = [  50],
+    base = [8],
+    lr = [0.001]
+    
+)
+##Data
+data_train = torch.tensor(np.load('../../data_kmc/2d_sets/train_set_80_20.npy'), dtype=torch.float32)
+data_test = torch.tensor(np.load('../../data_kmc/2d_sets/test_set_80_20.npy'), dtype=torch.float32)
+
+
+time = torch.logspace(-8, -4, 1000)
+##combine timesteps and config dim for training of the ae model
+data = torch.reshape(data_train,(data_train.shape[0]*data_train.shape[1],1,50,100))
+##Normalize data
+data = data/torch.mean(data)
+
+train_params = get_config("../../data_kmc/2d_sets/train_set_80_20_list.txt").to(device)##epsr, cbulk, v_bias
+##Rescale parameters with the mean
+params_scale = torch.mean(train_params, dim=0)
+train_params = train_params/params_scale
+
+
+train_params = torch.repeat_interleave(train_params, 1000, dim=0)
+time_train = torch.repeat_interleave(time, 51, dim=0) #repeat for each config --> 51 in training
+data  =torch.utils.data.TensorDataset(data, train_params, time_train)
+
+data_train, data_val = torch.utils.data.random_split((data), [40000, 11000])##Corresponds almost to 80/20 split and 
+#ensures that the configs are not split within
+
+train_loader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(data_val, batch_size=10200, shuffle=True)
 
 
 
-#combine timesteps and config dim for training of the ae model
-data = (np.reshape(data,(data.shape[0]*data.shape[1],1,50,100)))#.squeeze()
-x_train, x_test = random_split(data, [int(0.8*data.shape[0]), int(0.2*data.shape[0])], generator=torch.Generator().manual_seed(42))
+time_test = torch.repeat_interleave(time, 13, dim=0) #repeat for each config --> 13 in training
+test_params = get_config("../../data_kmc/2d_sets/test_set_80_20_list.txt")
+test_params = torch.repeat_interleave(test_params, 1000, dim=0)
+data_test = (np.reshape(data_test,(data_test.shape[0]*data_test.shape[1],1,50,100)))
+data_test  =torch.utils.data.TensorDataset(data_test, test_params, time_test)
+test_loader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, shuffle=True)
 
 
-encoder = pro_encoder2d(part_class,base, latent_dim)
-decoder = pro_decoder2d(part_class,base, latent_dim)
-model = VAE(encoder, decoder, latent_dim=latent_dim)
-model.to(device)
+##Create model
 
 
-# TODO: create data loader with training data and batch size
-train_loader = torch.utils.data.DataLoader(x_train, batch_size=batch_size, 
-                                           shuffle=True)
-#TODO: choose loss function and optimizer
 criterion = nn.MSELoss(reduction='sum')
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-#TODO: train the model
-model.train()
-train_vae(x_train, model, criterion, optimizer, train_loader, num_epochs=1000)
-#model.cpu() # move the model to the cpu
-torch.save(model.state_dict(), 'model_vae_kmc_red_data_e1000.pth')
-model.eval()
-model.load_state_dict(torch.load('model_vae_kmc_red_data_e1000.pth'))
 
 
-test_loader = torch.utils.data.DataLoader(x_test, batch_size=batch_size, shuffle=True)
-loss = 0
-for i, data in enumerate(test_loader):
+## hyper parameter loop
+
+for run_id, (latent_dim, base, lr) in enumerate(product(*parameters.values())):
+    encoder = pro_encoder2d(part_class,base, latent_dim)
+    decoder = pro_decoder2d(part_class,base, latent_dim)
+    model = VAE(encoder, decoder, latent_dim=latent_dim)
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    comment = f' latent_dim={latent_dim} base={base} lr={lr}'
+    tb = SummaryWriter(comment=comment) # tensorboard writer
+    model.train()
+    train_loss, train_KLD, best_model = train_vae_tb(model, criterion, optimizer, train_loader, val_loader, num_epochs=num_epochs, tb=tb)
+    model.cpu() # move the model to the cpu
+    torch.save(best_model, 'model_vae_p.pth')
+    model.eval()
+    model.load_state_dict(torch.load('model_vae_p.pth', map_location=device))
+    test_loss = 0
+    for x_test, params, time in test_loader:
             # Get the inputs; data is a list of [inputs, labels]
             #data = data.unsqueeze(1)
-            data = data.to(device)
-            decoded, _,_,_ = model(data)
-            loss += criterion(decoded, data)#+ (torch.sum(y_train!=y_pred))
+            #x_test = data.to(x_test)
+            decoded, _,_,_ = model(x_test)
+            test_loss += criterion(decoded, x_test)#+ (torch.sum(y_train!=y_pred))
+    
+    test_loss = test_loss/len(test_loader.dataset)
+    print("test loss", test_loss.item())
+    #tb.add_hparams(
+    #{"latent_dim": latent_dim, "base": base, "lr": lr},{"Train loss": train_loss, "Train KLD": train_KLD,  "Test loss": test_loss.item()}
+    #)
+tb.close()
 
 
-loss = loss/(0.2*data.shape[0])  
-print('Test loss:', loss.item())
+
