4cdf314d972173e5853e4fc6f1a215bb01164353
Marco Jochum <marco.jochum@tum.de>
2024-08-20 16:26:13 +0200

    added reshaping functions for vae usage

--- project/lib/helper.py
+++ project/lib/helper.py
@@ -25,3 +25,23 @@ def flat(list_2D):
     flatten_list = list(chain.from_iterable(list_2D))
     flatten_list = torch.tensor(flatten_list)
     return flatten_list
+
+
+def reshape_vae(x):
+    if len(x.shape) == 5:
+        x = torch.reshape(x, (x.shape[0]* x.shape[1],  1, 50, 100))
+    elif len(x.shape) == 6:
+        x = torch.reshape(x, (x.shape[0]*x.shape[1]*x.shape[2], 1, 50, 100))
+    elif len(x.shape) == 3:
+        x = torch.reshape(x, (x.shape[0]*x.shape[1], -1))
+    else:
+        print("Invalid shape:", x.shape)
+        return None
+    return x
+
+def unshape_vae(x, n_configs, n_time, lat):
+    if lat:
+        x = torch.reshape(x, (n_configs, n_time, -1))
+    else:
+        x = torch.reshape(x, (n_configs, n_time, 50,100))
+    return x
