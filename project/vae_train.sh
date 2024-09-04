1894c8ceb38ecd9717ec6b2685971df3ee26fb42
Marco Jochum <marco.jochum@tum.de>
2024-09-04 10:33:45 +0200

    sbatch for vae

---
+++ project/vae_train.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --partition=gpucloud
+#SBATCH --ntasks=1              # Requested (MPI)tasks. Default=1
+#SBATCH --cpus-per-task=10       # Requested CPUs per task. Default=1
+#SBATCH --ntasks-per-node=1     # Requested tasks per node. Default=1
+#SBatch --nodes=1
+#SBATCH --gpus=1                # Number of GPUs to use
+#SBATCH --mem-per-cpu=1G      # Memory limit per CPU. [1-999][K|M|G|T]
+#SBATCH --time=4-12:00:00         # Time limit. [[days-]hh:]mm[:ss]
+
+
+### configure file to store console output.
+### Write output to /dev/null to discard output
+#SBATCH --output=vae_p.log
+
+### configure email notifications
+### mail types: BEGIN,END,FAIL,TIME_LIMIT,TIME_LIMIT_90,TIME_LIMIT_80
+#SBATCH --mail-user=marco.jochum@tum.de
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT,TIME_LIMIT_90
+
+### give your job a name (and maybe a comment) to find it in the queue
+#SBATCH --job-name="vae_p"
+#SBATCH --comment="Train VAE with parameters"
+
+
+
+
+# Define the new output path
+
+
+# Use jq to modify the output_path in the input.json file
+
+
+### load environment modules
+source ../../thesis_env/bin/activate
+module load python/3.9-torch2-cuda12
+module load gcc/13.2.0
+module load oneapi/tbb/2021.7.0
+module load openmpi/4.1.4
+
+srun python train_vae_p.py
