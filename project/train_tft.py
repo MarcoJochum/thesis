27c80843910f19e2594e1bd75f6a6df4d36e515c
Marco Jochum <marco.jochum@tum.de>
2024-09-26 13:21:05 +0200

    Added loss logging, val series, spec lr

--- project/train_tft.py
+++ project/train_tft.py
@@ -9,18 +9,22 @@ from config.tft import Tft_config
 from NNs.autoencoder import *
 from lib.helper import *
 import warnings
+import time
+import pytorch_lightning as pl
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 warnings.filterwarnings("ignore")
-import logging
-from pytorch_lightning.utilities.model_summary import ModelSummary
-logging.disable(logging.CRITICAL)
+
+
+
+config_opt_tft_study_4 = Tft_config()
 
 
 
 
-VAE= torch.load("models/model_vae_lin.pth", map_location=torch.device("cpu"))
+VAE= torch.load("models/model_vae_lin_lat_10.pth", map_location=torch.device("cpu"))
 
 data_train = Tft_config.data_train
+data_train = data_train/ torch.mean(data_train)
 data_train = data_train[:,:Tft_config.n_steps]
 train_params = Tft_config.train_params
 
@@ -36,10 +40,14 @@ with torch.no_grad():
 series_list = []    
 for i in range(data_train.shape[0]):
     stat_cov = pd.DataFrame(data ={"epsr": [float(train_params[i,0])], "conc": [float(train_params[i,1])], "V_bias": [float(train_params[i,2])]})
-    series = TimeSeries.from_values(data_train[i].numpy())#, static_covariates=stat_cov)#all training data convertd to time series type
+    series = TimeSeries.from_values(data_train[i].numpy(), static_covariates=stat_cov)#all training data convertd to time series type
     series_list.append(series)
-print("series list 0 ", series_list[0])
 
+loss_logger = LossLogger()
+
+
+train_series = series_list[:Tft_config.train_size]
+val_series = series_list[Tft_config.train_size:]
 my_model = TFTModel(
     input_chunk_length=Tft_config.seq_length,
     output_chunk_length=Tft_config.pred_length,
@@ -51,17 +59,32 @@ my_model = TFTModel(
     n_epochs=Tft_config.n_epochs,
     add_relative_index=True, #this needs to be true becaue i do not have covariant data
     add_encoders=None,
-    likelihood=QuantileRegression(
+    optimizer_kwargs={"lr": Tft_config.lr},
+    model_name="lat_10_opt",
+    likelihood= QuantileRegression(
         quantiles=Tft_config.quantiles
     ),  # QuantileRegression is set per default
-    # loss_fn=MSELoss(),
+    
+    #loss_fn=torch.nn.MSELoss(),
     random_state=42,
     force_reset=True,
     pl_trainer_kwargs={
        "accelerator": "gpu",
-       "devices": [0],   
+       "devices": [0],
+       "callbacks": [loss_logger],
+       
     },
 )
+start_time = time.time()
+my_model.fit(series=train_series, val_series=val_series, verbose=True)
 
-my_model.fit(series_list, verbose=True)
-my_model.save("models/tft_model_lb_no_stat_cov.pt")
+
+end_time = time.time()
+print("Training time:", end_time-start_time)
+print("loss_logger.train_loss", loss_logger.train_loss)
+print("loss_logger.val_loss", loss_logger.val_loss)
+train_loss = np.array(loss_logger.train_loss)
+val_loss = np.array(loss_logger.val_loss)
+np.save("models/study_4/study_4_opt_qr_train_loss.npy", train_loss)
+np.save("models/study_4study_4_opt_qr_val_loss.npy", val_loss)
+my_model.save("models/study_4/tft_study_4_opt_qr.pt")
