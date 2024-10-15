3c5adb8590f2a096961314d79a677df10c17c40a
Marco Jochum <marco.jochum@tum.de>
2024-10-15 10:17:04 +0200

    added avg/std switch

--- project/train_tft.py
+++ project/train_tft.py
@@ -16,7 +16,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 warnings.filterwarnings("ignore")
 
 
-
+n_steps = 300
 config_opt_tft_study_4 = Tft_config()
 
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -27,11 +27,13 @@ VAE= torch.load("models/model_vae_"+ args.data_type +"_final.pt", map_location=t
 
 if args.data_type == 'avg':
     data_train = Tft_config.data_train_avg
+    train_params = Tft_config.train_params_avg
+
 elif args.data_type == 'std':
     data_train = Tft_config.data_train_std
+    train_params = Tft_config.train_params_std
 data_train = data_train/ torch.mean(data_train)
-data_train = data_train[:,:Tft_config.n_steps]
-train_params = Tft_config.train_params
+data_train = data_train[:,:n_steps]
 
 
 
@@ -44,12 +46,11 @@ with torch.no_grad():
 
 series_list = []    
 for i in range(data_train.shape[0]):
-    stat_cov = pd.DataFrame(data ={"epsr": [float(train_params[i,0])], "conc": [float(train_params[i,1])], "V_bias": [float(train_params[i,2])]})
+    stat_cov = pd.DataFrame(data ={"epsr": [float(train_params[i,0])], "conc": [np.log10(float(train_params[i,1]))], "V_bias": [float(train_params[i,2])]})
     series = TimeSeries.from_values(data_train[i].numpy(), static_covariates=stat_cov)#all training data convertd to time series type
     series_list.append(series)
 
 loss_logger = LossLogger()
-
 
 train_series = series_list[:Tft_config.train_size]
 val_series = series_list[Tft_config.train_size:]
@@ -91,8 +92,8 @@ print("loss_logger.train_loss", loss_logger.train_loss)
 print("loss_logger.val_loss", loss_logger.val_loss)
 train_loss = np.array(loss_logger.train_loss)
 val_loss = np.array(loss_logger.val_loss)
-np.save("models/study_5/study_5_opt_qr_train_loss_"+ args.data_type +".npy", train_loss)
-np.save("models/study_5/study_5_opt_qr_val_loss_"+ args.data_type +".npy", val_loss)
+np.save("models/no_sv/study_5_opt_qr_train_loss_"+ args.data_type +"no_sv_300_steps.npy", train_loss)
+np.save("models/no_sv/study_5_opt_qr_val_loss_"+ args.data_type +"no_sv_300_steps.npy", val_loss)
 
 best_model = my_model.load_from_checkpoint(model_name="lat_10_opt", best=True)
-best_model.save("models/study_5/tft_study_5_opt_qr_"+ args.data_type +".pt")
+best_model.save("models/no_sv/tft_study_5_opt_qr_"+ args.data_type +"_no_sv_300_steps.pt")
