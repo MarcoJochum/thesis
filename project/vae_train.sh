9af1baab0c91250de4f0ff7d33cbb97521f5c819
Marco Jochum <marco.jochum@tum.de>
2024-10-28 15:19:16 +0100

    switched to standard vae script

--- project/vae_train.sh
+++ project/vae_train.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --partition=gpucloud
+#SBATCH --partition=wom
 #SBATCH --ntasks=1              # Requested (MPI)tasks. Default=1
 #SBATCH --cpus-per-task=10       # Requested CPUs per task. Default=1
 #SBATCH --ntasks-per-node=1     # Requested tasks per node. Default=1
 #SBatch --nodes=1
 #SBATCH --gpus=1                # Number of GPUs to use
-#SBATCH --mem-per-cpu=1G      # Memory limit per CPU. [1-999][K|M|G|T]
+#SBATCH --mem-per-cpu=5G      # Memory limit per CPU. [1-999][K|M|G|T]
 #SBATCH --time=4-12:00:00         # Time limit. [[days-]hh:]mm[:ss]
 
 
 ### configure file to store console output.
 ### Write output to /dev/null to discard output
-#SBATCH --output=vae_p.log
+#SBATCH --output=logs/vae_std_hyper.log
 
 ### configure email notifications
 ### mail types: BEGIN,END,FAIL,TIME_LIMIT,TIME_LIMIT_90,TIME_LIMIT_80
@@ -19,8 +19,8 @@
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT,TIME_LIMIT_90
 
 ### give your job a name (and maybe a comment) to find it in the queue
-#SBATCH --job-name="vae_p"
-#SBATCH --comment="Train VAE with parameters"
+#SBATCH --job-name="vae_std_hyper"
+#SBATCH --comment="VAE hyperparameter search"
 
 
 
@@ -38,4 +38,4 @@ module load gcc/13.2.0
 module load oneapi/tbb/2021.7.0
 module load openmpi/4.1.4
 
-srun python train_vae_p.py
+srun python train_vae.py
